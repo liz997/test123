f507171cd87a3ad25f3513096d3e71d27e272ed6
liz997 <liz_337@hotmail.com>
2017-03-18 13:32:38 -0400

    test

---
+++ NunitDemo/NunitLib/Class1.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using NunitDemo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NunitLib
+{ [TestFixture]
+    public class Class1
+    {
+        Calc cal;
+
+        [SetUp]
+        public void Initialize()
+        {
+            cal = new Calc();
+        }
+
+        [Test]
+        public void TestAdd()
+        {
+
+            Assert.AreEqual(5, cal.Add(2, 3));
+        }
+
+        [Test]
+        public void TestAdd1()
+        {
+
+            Assert.AreEqual(5, cal.Add(2, 3));
+        }
+
+        [TearDown]
+        public void Destroy()
+        {
+            cal = null;
+        }
+    }
+}
